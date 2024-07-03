36d1d9b88e2a0b5ddfe1c254bf14e82a7c349e64
tien.bh <tienbhph15160@fpt.edu.vn>
2024-07-03 09:03:15 +0700

    test branch

--- routes/api.php
+++ routes/api.php
@@ -15,6 +15,7 @@ Route::apiResource('banners', BannerController::class);
 Route::apiResource('categories', CategoryController::class);
 
 Route::post('/categories/restore/{id}', [CategoryController::class, 'restore']);
+// Route::post('/categories/restore/{id}', [CategoryController::class, 'restore']);
 
 // Route::get('/categories', function () {
 //     $data = [
